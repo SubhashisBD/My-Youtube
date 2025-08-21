50ef1160ec7241a713abc835d44b098cfa0a87ed
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-21 23:55:57 +0530

    constant

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyD0Aqh_ruKxptIfxadQ5MhWXtE0bBLDe44";
+
+export const YOUTUBE_VIDE0_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
