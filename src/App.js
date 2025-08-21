ae4f3f113047ba6b5ae27229327dbcdd95790aaa
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-21 23:52:14 +0530

    Main app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-
+import { Provider } from "react-redux";
 import './App.css';
+import Body from "./components/Body";
+import Head from "./components/Head";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />
+      },
+      {
+        path: "watch",
+        element: <WatchPage />
+      }
+    ]
+
+  }
+])
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">Hello world</h1>
-    </div>
+    <Provider store={store}>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter}/>
+      </div>
+    </Provider>
   );
 }
 
