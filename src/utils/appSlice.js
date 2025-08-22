6bdea537b802a520c08a03b5d39f1644cd5acc23
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-22 11:58:14 +0530

    added closeMenu

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const appSlice = createSlice({
-    name:"app",
+    name: "app",
     initialState: {
-        isMenuOpen : true,
+        isMenuOpen: true,
     },
-    reducers:{
-        toggleMenu: (state)=>{
+    reducers: {
+        toggleMenu: (state) => {
             state.isMenuOpen = !state.isMenuOpen;
+        },
+        closeMenu: (state) => {
+            state.isMenuOpen = false;
         }
     }
 })
 
-export const {toggleMenu} = appSlice.actions;
+export const { toggleMenu, closeMenu } = appSlice.actions;
 export default appSlice.reducer;
